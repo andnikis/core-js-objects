08cabf31a5151bd81d0e253b8dee8337c4656d1b
andnikis <andreymon@gmail.com>
2024-03-16 15:28:10 +0600

    feat: do mergeObjects task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,17 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((acc, cur) => {
+    Object.entries(cur).forEach(([key, value]) => {
+      if (acc[key] !== undefined) {
+        acc[key] += value;
+      } else {
+        acc[key] = value;
+      }
+    });
+    return acc;
+  }, {});
 }
 
 /**
