e5cebd34afefde4fb28e147e724956bf3b3e74de
andnikis <andreymon@gmail.com>
2024-03-18 00:38:58 +0600

    feat: do next three tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -130,8 +130,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -144,8 +144,17 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const word = [];
+  const entries = Object.entries(lettersObject);
+  for (let i = 0; i < entries.length; i += 1) {
+    const entry = entries[i];
+    for (let wordIndex = 0; wordIndex < entry[1].length; wordIndex += 1) {
+      const [letter, positions] = entry;
+      word[positions[wordIndex]] = letter;
+    }
+  }
+  return word.join('');
 }
 
 /**
@@ -162,8 +171,30 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let money25 = 0;
+  let money50 = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      money25 += 1;
+    } else if (queue[i] === 50) {
+      if (money25 === 0) {
+        return false;
+      }
+      money25 -= 1;
+      money50 += 1;
+    } else if (queue[i] === 100) {
+      if (money25 > 0 && money50 > 0) {
+        money25 -= 1;
+        money50 -= 1;
+      } else if (money25 >= 3) {
+        money25 -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
